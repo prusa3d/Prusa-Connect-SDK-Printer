c4ea6a39af19a88b30386523f4395c02d107bf64
Ondrej Tuma <mcbig@zeropage.cz>
2020-08-13 12:11:47 +0200

    fix setup dependency
    * additional package info

--- prusa/connect/printer/pkg_info.py
+++
@@ -1,8 +0,0 @@
-__version__ = "0.1.0"
-
-description = "Python printer library for Prusa Connect"
-author = "Ondřej Tůma"
-author_email = "ondrej.tuma@prusa3d.cz"
-maintainer = "Ondrej Tuma"
-maintainer_email = "ondrej.tuma@prusa3d.cz"
-url = "https://github.com/prusa3d/Prusa-Connect-SDK-Printer"
