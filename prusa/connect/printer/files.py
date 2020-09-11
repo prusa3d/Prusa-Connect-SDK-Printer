627b3ad5c47104d9878ef51a26561a22a1c6f0c8
Martin Užák <martin.uzak@gmail.com>
2020-09-11 11:13:25 +0200

    Add inotify functionality.

--- prusa/connect/printer/files.py
+++ prusa/connect/printer/files.py
@@ -2,11 +2,19 @@
 
 import os
 import typing
+from datetime import datetime
+
+from inotify_simple import INotify, flags   # type: ignore
+
+from . import log
+
 
 # pylint: disable=fixme
+# pylint: disable=invalid-name
 # pylint: disable=redefined-builtin
 # pylint: disable=too-few-public-methods
-# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=unused-argument
 
 
 class File:
@@ -65,6 +73,26 @@ class File:
             last = node
         return last
 
+    def _abs_parts(self, result):
+        # pylint: disable=protected-access
+        if self.parent:
+            result.insert(0, self.name)
+            self.parent._abs_parts(result)
+        return result
+
+    def abs_parts(self):
+        """Return all the parts until root"""
+        result = []
+        return self._abs_parts(result)
+
+    def abs_path(self, path_storage: str):
+        """Return the absolute path of this File
+
+        :param path_storage: path on the storage to be used for creation
+            of the absolute path to this File.
+        """
+        return os.path.join(path_storage, os.sep.join(self.abs_parts()))
+
     def delete(self):
         """Delete this node"""
         del self.parent.children[self.name]
@@ -116,16 +144,37 @@ class File:
     def __str__(self):
         return self.name
 
+    def set_attrs(self, abs_path):
+        """Set attributes on this file according to `abs_path` file on storage.
+        """
+        stats = os.stat(abs_path)
+        self.attrs["ro"] = not os.access(abs_path, os.W_OK)
+        if not self.dir:
+            self.attrs["size"] = stats.st_size
+        m_datetime = datetime.fromtimestamp(stats.st_mtime)
+        m_time = datetime.timetuple(m_datetime)[:6]
+        self.attrs["m_time"] = m_time
+
 
 class Mount:
     """Represent a mountpoint"""
-    def __init__(self, tree, fs_sync=False):
+
+    def __init__(self, tree, mountpoint, abs_path_storage):
+        """
+        Initialize a Mount.
+
+        :param tree: tree of File instances
+        :param mountpoint: mount point on the virtual FS
+        :param abs_path_storage: absolute path on the physical storage
+        """
         self.tree = tree
-        self.fs_sync = fs_sync
+        self.mountpoint = mountpoint
+        self.path_storage = abs_path_storage
 
 
 class InvalidMountpointError(ValueError):
-    pass
+    """Mountpoint is not valid"""
+    ...
 
 
 class Filesystem:
@@ -136,13 +185,14 @@ class Filesystem:
 
     def __init__(self, sep="/"):
         self.sep = sep
-        self.mounts = dict()
+        self.mounts = dict()        # FS-mountpoint:Mount(...)
 
-    def mount(self, name: str, tree: File):
+    def mount(self, name: str, tree: File, storage_path: str = None):
         """Mount the a tree under a mountpoint.
 
         :param name: The mountpoint
         :param tree: The tree of `File` instances to be mounted
+        :param storage_path: Path on storage, if any
         :raises InvalidMountpointError: If the mountpoint is already used,
             or when it contains `self.sep` or when the `name` is empty or
             `self.sep` only.
@@ -159,7 +209,7 @@ class Filesystem:
         if name in self.mounts:
             raise InvalidMountpointError(f"`{name}` is already used")
 
-        self.mounts[name] = Mount(tree)
+        self.mounts[name] = Mount(tree, name, storage_path)
 
     def umount(self, name: str):
         """Umount a mountpoint.
@@ -211,7 +261,10 @@ class Filesystem:
             dirpath += os.path.sep
 
         # create nodes
-        root = File(os.path.dirname(dirpath).strip(self.sep), dir=True)
+        name = os.path.dirname(dirpath)
+        name = os.path.split(name)[1]
+        root = File(name, dir=True)
+        root.set_attrs(dirpath)
 
         for abs_dir, dirs, files in os.walk(dirpath):
             dirname = abs_dir[len(dirpath):]
@@ -221,9 +274,110 @@ class Filesystem:
                 parent = root.get(dirname.split(os.path.sep))
 
             for name in dirs:
-                parent.add(name, dir=True)
+                node = parent.add(name, dir=True)
+                node.set_attrs(os.path.join(abs_dir, name))
             for name in files:
-                parent.add(name)
+                node = parent.add(name)
+                node.set_attrs(os.path.join(abs_dir, name))
 
         # mount
-        self.mount(mountpoint, root)
+        self.mount(mountpoint, root, dirpath)
+
+
+class InotifyHandler:
+    """This is handler is initialised with a Filesystem instance and
+    using it makes sure that all its mounts `tree`s are updated on changes
+    on the physical storage"""
+
+    WATCH_FLAGS = flags.CREATE | flags.DELETE | flags.MODIFY | \
+        flags.DELETE_SELF | flags.MOVED_TO | flags.MOVED_FROM | \
+        flags.MOVE_SELF | flags.UNMOUNT
+
+    def __init__(self, fs: Filesystem):
+        self.fs = fs
+        self.inotify = INotify()
+        self.wds: typing.Dict[int, str] = {}       # watch descriptors
+        # init mount watches
+        for mount in self.fs.mounts.values():
+            self._init_wd(mount.path_storage, mount.tree)
+
+    def _init_wd(self, path_storage, node):
+        abs_dir = os.path.join(path_storage, os.sep.join(node.abs_parts()))
+        try:
+            wd = self.inotify.add_watch(abs_dir, self.WATCH_FLAGS)
+            self.wds[wd] = abs_dir
+            log.debug("Added watch (%s) for %s", wd, abs_dir)
+            for n in node.children.values():
+                if n.dir:
+                    self._init_wd(path_storage, n)
+        except PermissionError:
+            pass
+
+    def __call__(self, timeout=0):
+        """Process inotify events. This picks the proper `process_$FLAG`
+        handler method and executes it with the absolute_path of the
+        affected file as argument.
+        """
+        for event in self.inotify.read(timeout=timeout):
+            for flag in flags.from_mask(event.mask):
+                # ignore not watched events
+                if not self.WATCH_FLAGS & flag:
+                    log.debug("Ignoring %s", flag.name)
+                    continue
+                handler = getattr(self, f"process_{flag.name}")
+                parent_dir = self.wds[event.wd]
+                abs_path = os.path.join(parent_dir, event.name)
+                print("Flag", flag.name, abs_path, event)
+                handler(abs_path, event)
+
+    # pylint: disable=inconsistent-return-statements
+    def mount_for(self, abs_path):
+        """Find the proper mount for the `path` in self.fs"""
+        for mount in self.fs.mounts.values():
+            if abs_path.startswith(mount.path_storage):
+                return mount
+
+    def _rel_path_parts(self, path, mount):
+        rel_path = path[len(mount.path_storage):]
+        return rel_path.split(self.fs.sep)
+
+    def process_CREATE(self, abs_path, event):
+        mount = self.mount_for(abs_path)
+        parts = self._rel_path_parts(abs_path, mount)
+        *parent, name = parts
+        is_dir = event.mask & flags.ISDIR
+        node = mount.tree.get(parent).add(name, dir=is_dir)
+        node.set_attrs(abs_path)
+        if is_dir:
+            # add inotify watch
+            self._init_wd(mount.path_storage, node)
+
+    def process_DELETE(self, abs_path, event):
+        mount = self.mount_for(abs_path)
+        parts = self._rel_path_parts(abs_path, mount)
+        node = mount.tree.get(parts)
+        if node:
+            node.delete()
+
+    def process_MODIFY(self, abs_path, event):
+        mount = self.mount_for(abs_path)
+        parts = self._rel_path_parts(abs_path, mount)
+        node = mount.tree.get(parts)
+        node.set_attrs(abs_path)
+
+    def process_MOVED_FROM(self, abs_path, event):
+        self.process_DELETE(abs_path, event)
+
+    def process_MOVED_TO(self, abs_path, event):
+        self.process_CREATE(abs_path, event)
+
+    def process_DELETE_SELF(self, abs_path, event):
+        mount = self.mount_for(abs_path)
+        mount.tree.children = dict()
+        mount.tree.attrs = dict()
+
+    def process_MOVE_SELF(self, abs_path, event):
+        self.process_DELETE_SELF(abs_path, event)
+
+    def process_UNMOUNT(self, abs_path, event):
+        self.process_DELETE_SELF(abs_path, event)
--- tests/test_files.py
+++ tests/test_files.py
@@ -1,7 +1,13 @@
 import pytest
+import tempfile
+import os
+import shutil
+import stat
+import sys
 
+from collections import namedtuple
 from prusa.connect.printer.files import File, Filesystem, \
-    InvalidMountpointError
+    InvalidMountpointError, InotifyHandler
 
 
 @pytest.fixture
@@ -19,6 +25,9 @@ def nodes():
 @pytest.fixture
 def fs_from_dir(mocker):
     fs = Filesystem()
+    stat_mock = os.stat_result((33188, 267912, 64768, 1, 0, 0, 3044,
+                               1599740701, 1596120005, 1596120005))
+    mocker.patch("os.stat", return_value=stat_mock)
     mocker.patch("os.path.abspath", return_value='/a')
     mocker.patch("os.walk", return_value=[
             ('/a', ['b', 'c'], ['1.txt']),
@@ -27,6 +36,38 @@ def fs_from_dir(mocker):
         ])
     fs.from_dir('/somewhere/on/the/disk/a', 'a')
     return fs
+
+
+InotifyFixture = namedtuple('InotifyFixture', ['path', 'handler', 'fs'])
+
+
+@pytest.fixture()
+def inotify(nodes):
+    """Create and cleanup the same structure as in `nodes` in a temporary
+    directory. This returns the path to the dir on storage, the Inotify
+    handler and filesystem as a tuple: (path, handler, filesystem).
+    """
+    def create_on_storage(root_dir, node):
+        parts = node.abs_parts()
+        parts.insert(0, root_dir)
+        path = os.path.sep.join(parts)
+        if node.dir:
+            if not os.path.exists(path):    # root node is already created
+                os.mkdir(path)
+        else:
+            open(path, "w").close()         # create file
+        for n in node.children.values():
+            create_on_storage(root_dir, n)
+    tmp_dir = tempfile.TemporaryDirectory()
+    create_on_storage(tmp_dir.name, nodes)
+
+    # mount storage:$tmp_dir as Filesystem:/test
+    fs = Filesystem()
+    fs.from_dir(tmp_dir.name, "test")
+    handler = InotifyHandler(fs)
+
+    yield InotifyFixture(tmp_dir.name, handler, fs)
+    del tmp_dir
 
 
 @pytest.fixture
@@ -95,6 +136,15 @@ class TestFile:
         assert str(d) == "directory"
         assert str(f) == "filename"
 
+    def test_abs_parts(self, nodes):
+        node = nodes.a.c["2.txt"]
+        assert node.abs_parts() == ["a", "c", "2.txt"]
+
+    @pytest.mark.skipif(sys.platform == "win",
+                        reason="UINX only tests (depends on path sep)")
+    def test_abs_path(self, nodes):
+        node = nodes.a.c["2.txt"]
+        assert node.abs_path("/test") == "/test/a/c/2.txt"
 
 
 class TestFilesystem:
@@ -132,6 +182,11 @@ class TestFilesystem:
         assert fs_from_dir.get("/a/c/2.txt")
         assert fs_from_dir.get("/a/c/3.txt")
 
+        # test root node
+        assert fs_from_dir.get("/a").dir is True
+        assert fs_from_dir.get("/a/").name == "a"
+        assert fs_from_dir.get("/a/").parent is None
+
     def test_get_root(self, fs):
         a = fs.get("a")
         assert a.name == "a"
@@ -152,3 +207,79 @@ class TestFilesystem:
                        {'type': 'DIR', 'path': 'c', 'children': [
                            {'type': 'FILE', 'path': '2.txt'},
                            {'type': 'FILE', 'path': '3.txt'}]}]}]}  # NOQA: E501
+
+
+class TestINotify:
+    def test_CREATE_file(self, inotify):
+        p = os.path.join(inotify.path, "simple.txt")
+        open(p, "w").close()
+        inotify.handler()
+        assert inotify.fs.get("/test/simple.txt")
+        assert inotify.fs.get("/test/does-not-exit.txt") is None
+
+    def test_CREATE_dir(self, inotify):
+        p = os.path.join(inotify.path, "directory")
+        os.mkdir(p)
+        inotify.handler()
+        d = inotify.fs.get("/test/directory")
+        assert d
+        assert d.dir
+
+        # test that a inotify watch has also been installed
+        file_path = os.path.join(p, "file.txt")
+        open(file_path, "w").close()
+        inotify.handler()
+        assert inotify.fs.get("/test/directory/file.txt")
+
+    def test_DELETE_file(self, inotify):
+        p = os.path.join(inotify.path, "simple.txt")
+        open(p, "w").close()
+        inotify.handler()
+        assert inotify.fs.get("/test/simple.txt")
+
+        os.unlink(p)
+        inotify.handler()
+        assert not inotify.fs.get("/test/simple.txt")
+        assert inotify.fs.get("/test/a/c/2.txt")
+
+    def test_DELETE_dir(self, inotify):
+        node = inotify.fs.get("/test/a/b")
+        path = node.abs_path(inotify.path)
+        assert inotify.fs.get("/test/a/b")
+        os.rmdir(path)
+        inotify.handler()
+        assert not inotify.fs.get("/test/a/b")
+
+    def test_DELETE_SELF(self, inotify):
+        # this also tests MOVE_SELF and UNMOUNT events
+        shutil.rmtree(inotify.path)
+        inotify.handler()
+        assert not inotify.fs.get("/test/a/1.txt")
+        assert not inotify.fs.get("/test/a/c")
+
+    def test_MOVE_file(self, inotify):
+        src = inotify.fs.get("/test/a/1.txt")
+        assert src
+        src_path = src.abs_path(inotify.path)
+
+        dst = inotify.fs.get("/test/a/c")
+        assert dst
+        dst_path = dst.abs_path(inotify.path)
+
+        shutil.move(src_path, dst_path)
+        inotify.handler()
+        assert inotify.fs.get("/test/a/c/1.txt")
+
+    def test_MODIFY_file(self, inotify):
+        node = inotify.fs.get("/test/a/1.txt")
+        assert node.attrs['size'] == 0
+        assert node.attrs['ro'] is False
+        path = node.abs_path(inotify.path)
+        with open(path, "a") as fh:
+            fh.write("Hello World")
+        os.chmod(path, stat.S_IREAD)
+
+        inotify.handler()
+        node = inotify.fs.get("/test/a/1.txt")
+        assert node.attrs['size'] == 11
+        assert node.attrs['ro'] is True
