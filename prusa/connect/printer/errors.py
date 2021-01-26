a8c162155f211329e7cfb43ed74e7959b238d25d
Martin Užák <martin.uzak@gmail.com>
2021-01-26 13:29:52 +0100

    Implement feedback from review

--- prusa/connect/printer/errors.py
+++ prusa/connect/printer/errors.py
@@ -15,40 +15,73 @@ class SDKConnectionError(SDKError):
 class SDKServerError(SDKError):
     """Connect Error.
 
-    When there is some HTTP Errors.
+    When there are some HTTP Errors.
     """
 
 
 class ErrorState:
-    """Model chained error states as doubly linked list"""
+    """Model chained error states as doubly linked list
+
+    >>> root = ErrorState("root", "long msg root")
+    >>> leaf = ErrorState("leaf", "long msg leaf", prev=root)
+    >>> leaf.ok is None
+    True
+    >>> leaf.next is None
+    True
+    >>> root.prev is None
+    True
+    >>> root.next is leaf and leaf.prev is root
+    True
+    >>> bool(leaf)
+    False
+    >>> root.ok = False
+    >>> root.ok is False
+    True
+    >>> leaf.ok = True
+    >>> bool(root) and bool(leaf)
+    True
+    >>> str(leaf)
+    'leaf: True'
+    """
     def __init__(self, name, long_msg, prev=None, short_msg=None):
         self.name = name
         self.prev = prev
         self.next = None
-        self._ok = False
+        self._ok = None  # None = unknown, True = OK, False = NOK
         self.long_msg = long_msg
         self.short_msg = short_msg or name
+
+    def __bool__(self):
+        """Shorthand for `self.ok`. NOTE that this returns False
+        if `self.ok` is None
+        """
+        return bool(self.ok)
 
     # pylint: disable=invalid-name
     @property
     def ok(self):
-        # pylint: disable=missing-function-docstring
+        """Return True if current state is OK, False if not, None if unknown
+
+        When setting, new `value` is propagated back (if it is True) or forward
+        (if False). For performance's sake this happens only if the current
+        value is different from new.
+        """
         return self._ok
 
     @ok.setter
     def ok(self, value: bool):
-        # pylint: disable=missing-function-docstring
         if value is self._ok:  # skip updating prev/next if there is no change
             return
         self._ok = value
-        if value and self.prev:
+        if value and self._prev is not None:
             self.prev.ok = value
-        if not value and self.next:
+        if not value and self.next is not None:
             self.next.ok = value
 
     @property
     def prev(self):
-        # pylint: disable=missing-function-docstring
+        """Return previous state in the state chain. The setter will also
+        set `prev.next` to `self`."""
         return self._prev
 
     @prev.setter
@@ -59,5 +92,3 @@ class ErrorState:
 
     def __str__(self):
         return f"{self.name}: {self.ok}"
-
-    __repr__ = __str__
