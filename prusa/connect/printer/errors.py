c724d9e8a9e1df42b9939e3df225d8a7e5035d84
Ondrej Tuma <mcbig@zeropage.cz>
2021-02-17 14:32:28 +0100

    No notification handler

--- prusa/connect/printer/errors.py
+++ prusa/connect/printer/errors.py
@@ -1,24 +1,6 @@
 """SDK Exceptions"""
 
 
-class SDKError(RuntimeError):
-    """Base SDK Error class"""
-
-
-class SDKConnectionError(SDKError):
-    """Connect Connection Error.
-
-    When there is some communication problem with Connect.
-    """
-
-
-class SDKServerError(SDKError):
-    """Connect Error.
-
-    When there are some HTTP Errors.
-    """
-
-
 # pylint: disable=too-many-instance-attributes
 class ErrorState:
     """Model chained error states as doubly linked list
