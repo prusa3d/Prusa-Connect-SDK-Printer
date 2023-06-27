e2efd7c2d6befc524e875465cd0df8183887e505
Tomáš Jozífek <jozifektomas@gmail.com>
2023-06-27 18:40:27 +0200

    Appease linters while removing metadata

--- tests/test_files.py
+++ tests/test_files.py
@@ -12,6 +12,7 @@ from unittest.mock import patch
 import pytest  # type: ignore
 from gcode_metadata import MetaData
 
+from gcode_metadata import MetaData
 from prusa.connect.printer import const
 from prusa.connect.printer.files import (
     File,
